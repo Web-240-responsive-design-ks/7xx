7d178955a01d3303ec340f61b75a4ce06e76edf6
kevinshimp <ks95307@ravens.racc.edu>
2018-04-29 16:13:14 -0400

    added snow.js to make snow fall randomly and added to css to stylize the snow making it visible.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 // JavaScript Document
 
 window.onload = function () {
+	
+	setTimeout(function () {
+        Snowflake.init(document.getElementById('snow'));
+    }, 500);
+
 	for (i = 0; i < 36; i++) {
 		let v = Math.floor(Math.random() * (150 - 0)) + 0;
 		let y = Math.floor(Math.random() * (window.innerWidth - 0)) + 100;
